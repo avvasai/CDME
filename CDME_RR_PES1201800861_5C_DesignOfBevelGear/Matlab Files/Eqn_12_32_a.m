e2863dc43a1e28413572a9ad622352f883ae91bd
avvasai <46778550+avvasai@users.noreply.github.com>
2021-08-26 01:27:57 +0530

    Delete CDME_RR_PES1201800861_5C_DesignOfBevelGear directory

--- CDME_RR_PES1201800861_5C_DesignOfBevelGear/Matlab Files/Eqn_12_32_a.m
+++
@@ -1,6 +0,0 @@
-function del_p = Eqn_12_32_a(ii)
-%  EQN_12_32_A Equation  12.32(a)
-% 
-% To find the angle of pinion
-del_p = atan(1/ii)*(180/pi);
-end
--- CDME_RR_PES1201800861_5C_DesignOfBevelGear/Matlab Files/Eqn_12_32_b.m
+++
@@ -1,6 +0,0 @@
-function del_g = Eqn_12_32_b(ii)
-%  EQN_12_32_B Equation  12.32(b)
-% 
-% To find the angle of gear
-del_g = atan(ii)*(180/pi);
-end
